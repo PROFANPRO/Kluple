e909ba32f5f310c370bb5b478c68881ea9b31744
PROFANPRO <111689110+PROFANPRO@users.noreply.github.com>
2025-08-31 09:35:56 +0300

    Delete src/components directory

--- src/components/SendTx.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { sendTransaction } from "../services/tonconnect";
-
-export default function SendTx() {
-  return <button onClick={sendTransaction}>Отправить транзакцию</button>;
-}
