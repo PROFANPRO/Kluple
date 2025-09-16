0439520e5cc8b5bd486d34887fe613657d77ac78
PROFANPRO <111689110+PROFANPRO@users.noreply.github.com>
2025-09-16 09:18:40 +0300

    Update check-wallet.js

--- api/check-wallet.js
+++ api/check-wallet.js
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_KEY // лучше service_role, чтобы мог читать без RLS
+  process.env.SUPABASE_SERVICE_ROLE_KEY // лучше service_role, чтобы мог читать без RLS
 );
 
 export default async function handler(req, res) {
