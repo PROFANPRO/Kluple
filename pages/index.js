0875d4a80ed768b0e114460edf6a82f68e8cfa6a
PROFANPRO <111689110+PROFANPRO@users.noreply.github.com>
2025-09-19 18:50:07 +0300

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-// pages/index.js
-export default function Home() {
-  return (
-    <div style={{ fontFamily: "sans-serif", textAlign: "center", marginTop: "50px" }}>
-      <h1>Backend is running 🚀</h1>
-      <p>Это тестовая страница. Ваши API-роуты теперь доступны.</p>
-    </div>
-  );
-}
