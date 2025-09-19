7bb11c5e3b260b5377d3b89e22b8ce9504ced9a7
PROFANPRO <111689110+PROFANPRO@users.noreply.github.com>
2025-09-19 19:52:52 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{color: "white", background: "#000", minHeight: "100vh"}}>
+      <h1>WC Game</h1>
+      <p>Frontend загружается из public/index.html</p>
+    </div>
+  );
+}
