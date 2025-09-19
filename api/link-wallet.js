715667bf9b90e3282171d8bcfad89fb6e2dca140
PROFANPRO <111689110+PROFANPRO@users.noreply.github.com>
2025-09-19 17:04:26 +0300

    Update link-wallet.js

--- api/link-wallet.js
+++ api/link-wallet.js
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_RULE_KEY // ✅ используем сервисный ключ
+  process.env.SUPABASE_SERVICE_ROLE_KEY // ✅ используем сервисный ключ
 );
 
 export default async function handler(req, res) {
